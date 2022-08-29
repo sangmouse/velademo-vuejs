1aee12d051882b98f4fb6f27a6e7154a187935f0
hiepdv <dvhiep1@cmcglobal.vn>
2022-08-29 10:08:26 +0700

    [hiepdv] update code not-found-page

--- landing-page/src/App.vue
+++ landing-page/src/App.vue
@@ -1,20 +1,16 @@
 <script lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import Header from './layout/header/Header.vue';
-import Footer from "./layout/footer/Footer.vue";
+  import { RouterLink, RouterView } from "vue-router";
+  import Header from "./layout/header/Header.vue";
+  import Footer from "./layout/footer/Footer.vue";
 
-
-export default {
-  components: {
-    Header,
-    Footer,
-  }
-}
-
+  export default {
+    components: {
+      Header,
+      Footer,
+    },
+  };
 </script>
 
 <template>
-  <Header />
   <RouterView />
-  <Footer />
 </template>
---
+++ landing-page/src/pages/not-found-page/NotFoundPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="Page">
+      <div class="Page-wrap">
+        <h3 class="Page-wrap__title">404</h3>
+        <h3 class="Page-wrap__title Page-wrap__title--sub">PAGE NOT FOUND</h3>
+        <p class="Page-wrap__text">
+          You may have mis-typed the URL. Or the page has been removed, had its
+          name changed, or is temporarily unavailabl Actually, there is nothing
+          to see here....
+        </p>
+        <RouterLink to="/">
+          <button type="submit" class="btn btn-dark Page-wrap__btn">
+            Back to home
+          </button>
+        </RouterLink>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+  import "./not-found-page.scss";
+</script>
