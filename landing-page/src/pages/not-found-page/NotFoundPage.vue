fd8789daf7c527f298e06616b1050cf40eb3c7d7
hiepdv <dvhiep1@cmcglobal.vn>
2022-08-29 14:14:35 +0700

    [hiepdv] fixbug code not-found-page

--- landing-page/src/pages/not-found-page/NotFoundPage.vue
+++ landing-page/src/pages/not-found-page/NotFoundPage.vue
@@ -6,7 +6,7 @@
           <h3 class="not-found-page__title not-found-page__title--sub">PAGE NOT FOUND</h3>
           <p class="not-found-page__desc">You may have mis-typed the URL. Or the page has been removed, had its name changed, or is temporarily unavailabl Actually, there is nothing to see here....</p>
           <RouterLink to="/">
-            <button type="button" class="btn btn-dark not-found-page__submit">Back to home</button>
+            <button type="button" class="btn btn-dark btn-submit">Back to home</button>
           </RouterLink>
       </div>
     </div>
