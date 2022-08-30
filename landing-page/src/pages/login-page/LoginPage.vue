8f8a0573135847f2f8d23525b933638ab3708d21
hahieu <hatrunghieu2209198@gmail.com>
2022-08-30 16:33:31 +0700

    [hahieu] update login form

--- landing-page/src/pages/login-page/LoginPage.vue
+++ landing-page/src/pages/login-page/LoginPage.vue
@@ -1,126 +1,130 @@
 <template>
-  <div class="container">
-    <div class="account">
-      <div class="account-nav">
-        <div class="account-nav-login" @click="handleLogin(1)">
-          <h4
-            class="account-nav-login__title"
-            v-bind:class="
-              this.numLogin === 2 ? '' : 'account-nav-login__title--active'
-            "
-          >
-            Login
-          </h4>
-        </div>
-        <div class="account-nav-create" @click="handleLogin(2)">
-          <h4
-            class="account-nav-create__title"
-            v-bind:class="
-              this.numLogin === 1 ? '' : 'account-nav-create__title--active'
-            "
-          >
-            Create Account
-          </h4>
-        </div>
-      </div>
-      <div class="account-main">
-        <div class="account-content" v-if="this.numLogin === 1">
-          <div class="account-alert__error account-alert__error--active">
-            <p>Incorrect email or password!.</p>
+  <div id="authen-page">
+    <div class="container">
+      <div class="account">
+        <div class="account-nav">
+          <div class="account-nav-login" @click="handleLogin(1)">
+            <h4
+              class="account-nav-login__title"
+              v-bind:class="
+                this.numLogin === 2 ? '' : 'account-nav-login__title--active'
+              "
+            >
+              Login
+            </h4>
           </div>
-          <div class="account-alert__success account-alert__success--active">
-            <p>Successfully!</p>
+          <div class="account-nav-create" @click="handleLogin(2)">
+            <h4
+              class="account-nav-create__title"
+              v-bind:class="
+                this.numLogin === 1 ? '' : 'account-nav-create__title--active'
+              "
+            >
+              Create Account
+            </h4>
           </div>
-          <form action="">
-            <div class="account-email">
-              <input
-                type="email"
-                class="form-control account-email-control"
-                required
-                autofocus
-                placeholder="Email"
-              />
+        </div>
+        <div class="account-main">
+          <div class="account-content" v-if="this.numLogin === 1">
+            <div class="account-alert__error account-alert__error--active">
+              <p>Incorrect email or password!.</p>
             </div>
-            <div class="account-password">
-              <input
-                v-bind:type="showPassword ? 'text' : 'password'"
-                class="form-control account-password-control"
-                required
-                placeholder="Password"
-              />
-              <div
-                class="account-password__showbtn"
-                @click="handleShowPassword"
-              >
-                <span class="showbtn" v-if="isPassword">Hide </span>
-                <span class="showbtn" v-if="!isPassword">Show</span>
+            <div class="account-alert__success account-alert__success--active">
+              <p>Successfully!</p>
+            </div>
+            <div>
+              <div class="account-email">
+                <input
+                  type="email"
+                  class="form-control account-email-control"
+                  required
+                  autofocus
+                  placeholder="Email"
+                />
+              </div>
+              <div class="account-password">
+                <input
+                  v-bind:type="showPassword ? 'text' : 'password'"
+                  class="form-control account-password-control"
+                  required
+                  placeholder="Password"
+                />
+                <div
+                  class="account-password__showbtn"
+                  @click="handleShowPassword"
+                >
+                  <span class="showbtn" v-if="isPassword">Hide </span>
+                  <span class="showbtn" v-if="!isPassword">Show</span>
+                </div>
+              </div>
+              <div class="account-signin-btn">
+                <input type="button" value="Sign In" class="signin-btn" />
               </div>
             </div>
-            <div class="account-signin-btn">
-              <input type="submit" value="Sign In" class="signin-btn" />
-            </div>
-          </form>
-        </div>
-
-        <div class="register-content" v-if="this.numLogin === 2">
-          <div class="register-alert__error register-alert__error--active">
-            <p>Check again please!</p>
-          </div>
-          <div class="register-alert__success register-alert__success--active">
-            <p>Create account successfully!</p>
           </div>
 
-          <form action="">
-            <div class="register-name">
-              <input
-                type="text"
-                class="form-control register-firstname-control"
-                placeholder="First Name"
-                required
-                autofocus
-              />
-              <input
-                type="text"
-                class="form-control register-lastname-control"
-                placeholder="Last Name"
-                required
-                autofocus
-              />
+          <div class="register-content" v-if="this.numLogin === 2">
+            <div class="register-alert__error register-alert__error--active">
+              <p>Check again please!</p>
             </div>
-            <div class="register-email">
-              <input
-                type="email"
-                class="form-control register-email-control"
-                required
-                placeholder="Email"
-              />
+            <div
+              class="register-alert__success register-alert__success--active"
+            >
+              <p>Create account successfully!</p>
             </div>
-            <div class="register-password">
-              <input
-                v-bind:type="showPassword ? 'text' : 'password'"
-                class="form-control register-password-control"
-                required
-                placeholder="Password"
-              />
-              <div
-                class="register-password__showbtn"
-                @click="handleShowPassword"
-              >
-                <span class="showbtn" v-if="isPassword">Hide</span>
-                <span class="showbtn" v-if="!isPassword">Show</span>
+
+            <div>
+              <div class="register-name">
+                <input
+                  type="text"
+                  class="form-control register-firstname-control"
+                  placeholder="First Name"
+                  required
+                  autofocus
+                />
+                <input
+                  type="text"
+                  class="form-control register-lastname-control"
+                  placeholder="Last Name"
+                  required
+                  autofocus
+                />
               </div>
-            </div>
-            <div class="register-create-btn">
-              <input
-                type="submit"
-                value="Create Account"
-                class="create-account-btn"
-              />
-            </div>
-          </form>
+              <div class="register-email">
+                <input
+                  type="email"
+                  class="form-control register-email-control"
+                  required
+                  placeholder="Email"
+                />
+              </div>
+              <div class="register-password">
+                <input
+                  v-bind:type="showPassword ? 'text' : 'password'"
+                  class="form-control register-password-control"
+                  required
+                  placeholder="Password"
+                />
+                <div
+                  class="register-password__showbtn"
+                  @click="handleShowPassword"
+                >
+                  <span class="showbtn" v-if="isPassword">Hide</span>
+                  <span class="showbtn" v-if="!isPassword">Show</span>
+                </div>
+              </div>
+              <div class="register-create-btn">
+                <input
+                  type="button"
+                  value="Create Account"
+                  class="create-account-btn"
+                />
+              </div>
+          </div>
         </div>
       </div>
     </div>
+  </div>
   </div>
 </template>
 
