b0fde6a7a23e9e374be35ee9ecff9565c838781f
nvsang3 <vansang261198@gmail.com>
2022-09-05 15:28:42 +0700

    [nvsang3] add code base

---
+++ admin/src/api/request.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8081";
+
+const http = axios.create({
+  baseURL: API_URL,
+});
+
+http.interceptors.request.use(
+  (config) => {
+    // Do something before request is sent
+    return { ...config };
+  },
+  (err) => {
+    // Do something with request errorgi
+    return Promise.reject(err);
+  }
+);
+
+http.interceptors.response.use(
+  (response) => {
+    // Do something with response data
+    return response.data;
+  },
+  (err) => {
+    // Do something with response error
+    return Promise.reject(err);
+  }
+);
+
+export default http;
